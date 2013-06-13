5ff09c76a8e6d7d9d2f42644d5cc609fb778ecdc
rac <m@rac.su>
2013-06-13 11:27:14 +0200

    slidedeck

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Page Object Pattern',
+    subtitle: 'and how to use selenium',
+    eventInfo: {
+      title: 'Android Developer Days',
+      date: 'June 14/15 2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/gdg_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['add2013'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Michel Racic',
+    company: 'GDG Zürich',
+    gplus: 'http://racic.ch/+',
+    //twitter: '@rac2030',
+    www: 'http://gdgzh.ch',
+    github: 'http://github.com/rac2030'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
