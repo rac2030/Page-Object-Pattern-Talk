b32ebd2d3178edad352d3e85b72721c231faf223
rac <m@rac.su>
2014-04-02 00:53:27 +0200

    set serve script to start on index.html

--- serve.sh
+++ serve.sh
@@ -19,4 +19,4 @@ else
   open=xdg-open
 fi
 
-$open http://localhost:$port/add13popt.html?presentme=true && python -m SimpleHTTPServer $port;
+$open http://localhost:$port/?presentme=true && python -m SimpleHTTPServer $port;
